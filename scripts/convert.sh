faf98113b95bf5e582d6a44d7c8076fc0e74bdb8
Kisturyo <honkiliang@gmail.com>
2024-04-20 10:04:30 +0800

    Update

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 for e in $(ls -d luci-*/po); do
-	if [[ -d $e/zh-cn ]]; then
+	if [ -h $e/zh-cn ]; then
+		rm -rf $e/zh-cn
+	elif [[ -d $e/zh-cn ]]; then
 		rm -rf $e/zh_Hans
 		mv $e/zh-cn $e/zh_Hans
-	elif [[ -d $e/zh-Hans ]]; then
-	    rm -rf $e/zh_cn
 	fi
 done
 
 for e in $(ls -d luci-*/po); do
-	if [[ -d $e/zh-tw ]]; then
+	if [ -h $e/zh-tw ]; then
+		rm -rf $e/zh-tw
+	elif [[ -d $e/zh-tw ]]; then
 		rm -rf $e/zh_Hant
 		mv $e/zh-tw $e/zh_Hant
-	elif [[ -d $e/zh-Hant ]]; then
-	    rm -rf $e/zh_tw
 	fi
 done
 
 find ./ -iname Makefile -exec sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' {} +
 find ./ -iname Makefile -exec sed -i 's#../../lang/golang/golang-package.mk#$(TOPDIR)/feeds/packages/lang/golang/golang-package.mk#g' {} +
-find ./ -iname tailscale.* -exec sed -i 's#nftables#iptables#g' {} +
+#find ./ -iname Makefile
--- scripts/upstream_main.sh
+++ scripts/upstream_main.sh
@@ -4,7 +4,7 @@
 # LuCI
 git clone --depth 1 --filter=blob:none --sparse https://github.com/immortalwrt/luci immortalwrt-luci
 cd immortalwrt-luci
-git sparse-checkout set applications/luci-app-ramfree applications/luci-app-dufs applications/luci-app-rclone applications/luci-app-zerotier
+git sparse-checkout set applications/luci-app-ramfree applications/luci-app-dufs applications/luci-app-rclone applications/luci-app-usb-printer applications/luci-app-wechatpush
 mv applications/* ../
 cd ../
 rm -rf immortalwrt-luci
@@ -17,23 +17,53 @@ mv net/* ../
 cd ../
 rm -rf immortalwrt-packages
 
+# ADGuardHome
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome
+
+# AutoUpdate
+git clone --depth 1 https://github.com/Hyy2001X/AutoBuild-Packages
+cd AutoBuild-Packages
+for i in "autoupdate" "luci-app-autoupdate"; do \
+  mv "$i" .. ; \
+done
+cd .. && rm -rf AutoBuild-Packages
+
+# CloudDreve
+git clone --depth 1 https://github.com/kiddin9/luci-app-cloudreve
+
+# CPUFreq
+git clone --depth 1 https://github.com/jjm2473/openwrt-apps
+cd openwrt-apps
+for i in "luci-app-cpufreq"; do \
+  mv "$i" .. ; \
+done
+cd .. && rm -rf openwrt-apps
+
+# FanControl
+git clone --depth 1 https://github.com/JiaY-shi/fancontrol
+cd fancontrol
+for i in "fancontrol" "luci-app-fancontrol"; do \
+  mv "$i" .. ; \
+done
+cd .. && rm -rf fancontrol
+
+
 # Tailscale
 # git clone --depth 1 https://github.com/Carseason/openwrt-tailscale
 # mv ./openwrt-tailscale/luci-app-tailscaler ./
 # rm -rf openwrt-tailscale
 
-# luci-theme-argon
-git clone --depth 1 https://github.com/GinkoCai/luci-theme-argon
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
+# Theme
 git clone --depth 1 https://github.com/kiddin9/luci-theme-edge
 
-# passwall
+# Passwall2
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2/ ./luci-app-passwall2
-mv ./luci-app-passwall/luci-app-passwall2/* ./luci-app-passwall2/
+mv ./luci-app-passwall2/luci-app-passwall2/* ./luci-app-passwall2/
 rm -rf ./luci-app-passwall2/luci-app-passwall2/
+sed -i 's/xray-core/sing-box/g' ./luci-app-passwall2/Makefile
 git clone --depth 1 --branch main --filter=blob:none --sparse https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages
 cd openwrt-passwall-packages && git sparse-checkout init --cone
-for i in "tcping" "xray-core" "sing-box"; do \
+for i in "tcping" "v2ray-geodata" "sing-box"; do \
   git sparse-checkout set "$i" && mv "$i" .. ; \
 done
 cd .. && rm -rf openwrt-passwall-packages
@@ -41,15 +71,10 @@ cd .. && rm -rf openwrt-passwall-packages
 # Sirpdboy
 git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus
-git clone --depth 1 https://github.com/sirpdboy/netspeedtest.git
-mv netspeedtest/homebox netspeedtest/luci-app-netspeedtest ./
-rm -rf netspeedtest
 git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go ddns
 mv ddns/*ddns-go  ./
 rm -rf ddns
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard
-git clone --depth 1 https://github.com/sirpdboy/luci-theme-kucat
-git clone --depth 1 https://github.com/selfcan/luci-app-homebox
 
 # WolPlus
 git clone --depth 1 https://github.com/animegasan/luci-app-wolplus
